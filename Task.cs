34ad5dbb8bffe0dc7acdf23197e38b6d4ded8503
Seth Ek <sge3141@gmail.com>
2025-05-08 14:24:29 -0500

    Template Down

--- Task.cs
+++ Task.cs
@@ -4,9 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+public enum TaskType
+{
+    Assignment,
+    Exam,
+    Quiz,
+    Project,
+    Custom
+}
+
+public enum Priority
+{
+    Moderate,
+    High,
+    Low
+}
+
 namespace TaskManager
 {
-    internal class Task
+    internal class Task // Model
     {
+        // Class fields 
+        private string title;
+        private TaskType type;
+        private string course;
+        private Priority priority;
+        private string description;
+        private DateOnly date;
+
+        // Class properties 
+        public string Title { get { return title; } set { title = value; } }
+        public string Course { get { return course; } set { course = value; } }
+        public Priority Priority { get { return priority; } set { priority = value; } } 
+        public string Description { get { return description; } set { description = value; } }
+        public DateOnly Date { get { return date; } set { date = value; } }
+
+        // Class constructor
+        public Task(string title, TaskType type, string course, Priority priority, string description, DateOnly date)
+        {
+            this.title = title;
+            this.type = type;
+            this.course = course;
+            this.priority = priority;
+            this.description = description;
+            this.date = date;
+        }
     }
 }
---
+++ TaskList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    class TaskList
+    {
+        // Class fields
+        private List<Task> list;
+
+        // Class constructor
+        public TaskList()
+        {
+            list = new List<Task>();
+        }
+
+
+    }
+}
